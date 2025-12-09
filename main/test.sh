f3341aae4229be625c38a98f9ebf75efc29970c4
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-09 09:40:12 +0900

    change setting

--- main/test.sh
+++ main/test.sh
@@ -1,4 +1,4 @@
-model_type="clap"
+model_type="method"
 training_dataset="audiocaps"
 dataset="esc50"
 
@@ -8,5 +8,5 @@ python -u test.py \
         --model_type ${model_type} \
         --dataset ${dataset} \
         --dropout_rate 0.1 \
-        --saved_model_path "./saved_models/train/${model_type}_${training_dataset}/epoch25.pth" \
+        --saved_model_path "./saved_models/train/${model_type}_${training_dataset}/epoch31.pth" \
         2>&1 | tee "logs/test/${model_type}_${training_dataset}/${dataset}.log"
