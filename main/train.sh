308583a2f8130392ec6dfe9438996b0454fbdeb7
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-09 10:15:16 +0900

    change learning rate 2 1e-3

--- main/train.sh
+++ main/train.sh
@@ -7,12 +7,12 @@ python -u train.py \
     --model_type ${model_type} \
     --seed 42 \
     --dataset ${dataset} \
-    --lr 1e-4 \
+    --lr 1e-3 \
     --epochs 40 \
     --batch_size 50 \
     --dropout_rate 0.1 \
     --hp_contrastive 0.2 \
-    --hp_sim 1.0 \
-    --hp_discrim 1.0 \
-    --hp_recon 1.0 \
+    --hp_sim 0.1 \
+    --hp_discrim 2.0 \
+    --hp_recon 2.0 \
     2>&1 | tee "logs/train/${model_type}_${dataset}/train.log"
