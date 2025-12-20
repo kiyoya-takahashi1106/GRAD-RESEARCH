52830c829fbbe8d0dcc14e3951a32c1f0e56648a
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-12-20 11:54:51 +0900

    add macsdata

--- main/datasets/fea_dataset.py
+++ main/datasets/fea_dataset.py
@@ -20,7 +20,7 @@ pt.fileの中身のイメージ
 
 
 class FeaDataset(Dataset):
-    def __init__(self, dataset: str, split: str, root: str = "../data"):
+    def __init__(self, dataset: str, split: str, hidden_dim: int, root: str = "../data"):
         self.dataset = dataset
         self.split = split
 
@@ -28,18 +28,18 @@ class FeaDataset(Dataset):
             if (split == 'all'):
                 self.splits = ['train', 'val', 'test']
             elif (split == "train"):
-                self.splits = ['train']
-            elif (split == "val"):
-                self.splits = ['val', 'test']
+                self.splits = ['train', 'val']   # trainの全部とvalの37個以外
+            elif (split == "val"): 
+                self.splits = ['val', 'test']   # valのうち前37個のみとtest全部
                 
         elif (dataset == "fsd50k"):
             # No test split Dataset 
             if (split == 'all'):
                 self.splits = ['train', 'val']
             elif (split == "train"):
-                self.splits = ['train', 'val']   # valの1500個以外はtrainに含まれる
+                self.splits = ['train', 'val']   # valの1000個以外はtrainに含まれる
             elif (split == "val"):
-                self.splits = ['val']            # valのうち前1500個のみ
+                self.splits = ['val']            # valのうち前1000個のみ
 
         elif (dataset == "clotho"):
             if (split == 'all'):
@@ -47,33 +47,54 @@ class FeaDataset(Dataset):
             elif (split == "train"):
                 self.splits = ['train', 'val', 'test']   # trainとvalの全部とtestの1000個以外はtrain
             elif (split == "val"):
-                self.splits = ['test']                   # testのうち前1000個のみ   
+                self.splits = ['test']                   # testのうち前1000個のみ
+
+        elif (dataset == "macs"):
+            if (split == 'all'):
+                self.splits = ['train']
+            elif (split == "train"):
+                self.splits = ['train']   # trainの1000個以外はtrain
+            elif (split == "val"):
+                self.splits = ['train']   # trainのうち前1000個のみ
+
         self.root = Path(root)
 
         self.samples = []
         for temp_split in self.splits:
-            self.get_feature(dataset, temp_split)
+            self.get_feature(dataset, temp_split, hidden_dim)
 
 
-    def get_feature(self, dataset: str, temp_split: str):
+    def get_feature(self, dataset: str, temp_split: str, hidden_dim: int):
         # ❶ ここで一度だけ埋め込みファイルを読み込む
-        fea_path = self.root / self.dataset / "fea.pt"
+        fea_path = self.root / self.dataset / f"fea{hidden_dim}.pt"
         file_content = torch.load(fea_path, map_location="cpu")
 
         # ❷ サンプルをまとめて self.samples に展開
         features_lst = file_content[temp_split]
 
+        if (dataset == "audiocaps"):
+            if (self.split == "train" and temp_split == "val"):
+                features_lst = features_lst[37:]   # valの37個以外はtrainに含まれる
+            elif (self.split == "val" and temp_split == "val"):
+                features_lst = features_lst[:37]   # valのうち前37個のみ
+
         if (dataset == "fsd50k"):
             if (self.split == "train" and temp_split == "val"):
-                features_lst = features_lst[1500:]   # valの1500個以外はtrainに含まれる
+                features_lst = features_lst[1000:]   # valの1000個以外はtrainに含まれる
             elif (self.split == "val" and temp_split == "val"):
-                features_lst = features_lst[:1500]   # valのうち前1500個のみ
+                features_lst = features_lst[:1000]   # valのうち前1000個のみ
 
         elif (dataset == "clotho"):
             if (self.split == "train" and temp_split == "test"):
                 features_lst = features_lst[1000:]   # testの1000個以外はtrainに含まれる
             elif (self.split == "val" and temp_split == "test"):
                 features_lst = features_lst[:1000]   # testのうち前1000個のみ
+
+        elif (dataset == "macs"):
+            if (self.split == "train" and temp_split == "train"):
+                features_lst = features_lst[1000:]   # trainの1000個以外はtrainに含まれる
+            elif (self.split == "val" and temp_split == "train"):
+                features_lst = features_lst[:1000]   # trainのうち前1000個のみ
 
         for text_emb, audio_emb in features_lst:
             self.samples.append((text_emb, audio_emb))
--- main/train.py
+++ main/train.py
@@ -35,15 +35,17 @@ def args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_type", type=str, help="clap or method")
     parser.add_argument("--seed", type=int)
-    parser.add_argument("--dataset", type=str, help="audiocaps or fsd50k or mix")
+    parser.add_argument("--dataset", type=str, help="audiocaps or fsd50k or clotho or macs or mix")
     parser.add_argument("--lr", type=float)
     parser.add_argument("--epochs", type=int)
     parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--hidden_dim", type=int, default=768)
     parser.add_argument("--dropout_rate", type=float)
     # hp
     parser.add_argument("--hp_contrastive", type=float)
     parser.add_argument("--hp_sim", type=float)
-    parser.add_argument("--hp_diff", type=float)
+    parser.add_argument("--hp_cp_diff", type=float)
+    parser.add_argument("--hp_pp_diff", type=float)
     parser.add_argument("--hp_recon", type=float)
     args = parser.parse_args()
     return args
@@ -53,16 +55,18 @@ def train(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if (args.model_type == "clap"):
         model = Clap(
+            hidden_dim=args.hidden_dim,
             dropout_rate=args.dropout_rate
         )
     elif (args.model_type == "method"):
         model = MethodModel(
+            hidden_dim=args.hidden_dim,
             dropout_rate=args.dropout_rate
         )
     
     # TensorBoard Writer設定
-    os.makedirs(f"runs/{args.model_type}_{args.dataset}", exist_ok=True)
-    log_dir = os.path.join("runs", f"{args.model_type}_{args.dataset}")
+    os.makedirs(f"runs/{args.hidden_dim}/{args.model_type}_{args.dataset}", exist_ok=True)
+    log_dir = os.path.join("runs", f"{args.hidden_dim}", f"{args.model_type}_{args.dataset}")
     writer = SummaryWriter(log_dir=log_dir)
     print(f"TensorBoard logs will be saved to: {log_dir}")
     
@@ -74,12 +78,19 @@ def train(args):
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=0)
 
     # データセットとデータローダーの準備
-    if (args.dataset == "audiocaps"  or  args.dataset == "fsd50k"  or  args.dataset == "clotho"):
-        train_dataset = FeaDataset(dataset=args.dataset, split='train')
-        val_dataset = FeaDataset(dataset=args.dataset, split='val')
+    if (args.dataset == "audiocaps"  or  args.dataset == "fsd50k"  or  args.dataset == "clotho"  or  args.dataset == "macs"):
+        train_dataset = FeaDataset(dataset=args.dataset, split='train', hidden_dim=args.hidden_dim)
+        val_dataset = FeaDataset(dataset=args.dataset, split='val', hidden_dim=args.hidden_dim)
     elif (args.dataset == "mix"):
-        train_dataset = ConcatDataset([FeaDataset(dataset="audiocaps", split='train'), FeaDataset(dataset="fsd50k", split='train'), FeaDataset(dataset="clotho", split='train')])
-        val_dataset = ConcatDataset([FeaDataset(dataset="audiocaps", split='val'), FeaDataset(dataset="fsd50k", split='val'), FeaDataset(dataset="clotho", split='val')])
+        dataset_names = ["audiocaps", "fsd50k", "clotho"]
+        train_dataset = ConcatDataset([
+            FeaDataset(dataset=name, split="train", hidden_dim=args.hidden_dim)
+            for name in dataset_names
+        ])
+        val_dataset = ConcatDataset([
+            FeaDataset(dataset=name, split="val", hidden_dim=args.hidden_dim)
+            for name in dataset_names
+        ])
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
 
@@ -128,8 +139,8 @@ def train(args):
                                                                                                                                 private_text, private_audio,
                                                                                                                                 recon_text, recon_audio
                                                                                                                             )
-                recon_loss = (recon_text_loss + recon_audio_loss) / 2
                 c2p_loss = (c2p_text_loss + c2p_audio_loss) / 2
+                recon_loss = (recon_text_loss + recon_audio_loss) / 2
 
             # recode loss
             contractive_loss = args.hp_contrastive * contractive_loss
@@ -140,15 +151,14 @@ def train(args):
             elif (args.model_type == "method"):
                 sim_loss = args.hp_sim * sim_loss * (epoch+1) / args.epochs
                 sim_loss_lst.append(sim_loss.item())
-                c2p_loss = args.hp_diff * c2p_loss
+                c2p_loss = args.hp_cp_diff * c2p_loss
                 c2p_loss_lst.append(c2p_loss.item())
-                p2p_loss = args.hp_diff * p2p_loss
+                p2p_loss = args.hp_pp_diff * p2p_loss
                 p2p_loss_lst.append(p2p_loss.item())
                 recon_loss = args.hp_recon * recon_loss
                 recon_loss_lst.append(recon_loss.item())
                 # 全体loss
                 loss = contractive_loss + sim_loss + c2p_loss + p2p_loss + recon_loss
-                # loss = contractive_loss + sim_loss + p2p_loss + recon_loss
                 loss_lst.append(loss.item())
 
             # if ((epoch == 0)):
@@ -173,22 +183,22 @@ def train(args):
         print(f"Epoch {epoch}")
         if (args.model_type == "method"):
             epoch_contractive_loss = sum(contractive_loss_lst) / len(contractive_loss_lst)
-            writer.add_scalars('Loss/Train/Epoch/contractive_Losses', {'Contractive': epoch_contractive_loss}, epoch)
+            writer.add_scalars('Loss/Train/contractive_Losses', {'Contractive': epoch_contractive_loss}, epoch)
             print(f"Contractive: {epoch_contractive_loss:.6f}")
             epoch_sim_loss = sum(sim_loss_lst) / len(sim_loss_lst)
-            writer.add_scalars('Loss/Train/Epoch/sim_Losses', {'Sim': epoch_sim_loss}, epoch)
+            writer.add_scalars('Loss/Train/sim_Losses', {'Sim': epoch_sim_loss}, epoch)
             print(f"Sim: {epoch_sim_loss:.6f}")
             epoch_c2p_loss = sum(c2p_loss_lst) / len(c2p_loss_lst)      
-            writer.add_scalars('Loss/Train/Epoch/c2p_Losses', {'C2P': epoch_c2p_loss}, epoch)
+            writer.add_scalars('Loss/Train/c2p_Losses', {'C2P': epoch_c2p_loss}, epoch)
             print(f"C2P: {epoch_c2p_loss:.6f}")
             epoch_p2p_loss = sum(p2p_loss_lst) / len(p2p_loss_lst)      
-            writer.add_scalars('Loss/Train/Epoch/p2p_Losses', {'P2P': epoch_p2p_loss}, epoch)
+            writer.add_scalars('Loss/Train/p2p_Losses', {'P2P': epoch_p2p_loss}, epoch)
             print(f"P2P: {epoch_p2p_loss:.6f}")
             epoch_recon_loss = sum(recon_loss_lst) / len(recon_loss_lst)
-            writer.add_scalars('Loss/Train/Epoch/recon_Losses', {'Reconstruction': epoch_recon_loss}, epoch)
+            writer.add_scalars('Loss/Train/recon_Losses', {'Reconstruction': epoch_recon_loss}, epoch)
             print(f"Reconstruction: {epoch_recon_loss:.6f}")
         epoch_loss = sum(loss_lst) / len(loss_lst)
-        writer.add_scalars('Loss/Train/Epoch/overall_Losses', {'Overall': epoch_loss}, epoch)
+        writer.add_scalars('Loss/Train/overall_Losses', {'Overall': epoch_loss}, epoch)
         print(f"OverALL: {epoch_loss:.6f}")
 
 
@@ -217,21 +227,21 @@ def train(args):
                 test_contractive_lst.append(contractive.item())
 
         epoch_test_contractive = sum(test_contractive_lst) / len(test_contractive_lst)
-        writer.add_scalars('Loss/Test/Epoch/Contrastive', {'Contrastive': epoch_test_contractive}, epoch)
+        writer.add_scalars('Loss/Test/Contrastive', {'Contrastive': epoch_test_contractive}, epoch)
         print(f"Test Contrastive: {epoch_test_contractive:.6f}")
         
         # モデル保存
         if (epoch_test_contractive < best_contractive):
             best_contractive = epoch_test_contractive
             os.makedirs(
-                f"saved_models/train/{args.model_type}_{args.dataset}/", exist_ok=True
+                f"saved_models/{args.hidden_dim}/{args.model_type}_{args.dataset}/", exist_ok=True
             )
             # best_model_path = (
             #     f"saved_models/train/{args.model_type}_{args.dataset}/"
             #     f"epoch{epoch}.pth"
             # )
             best_model_path = (
-                f"saved_models/train/{args.model_type}_{args.dataset}/"
+                f"saved_models/{args.hidden_dim}/{args.model_type}_{args.dataset}/"
                 f"best.pth"
             )
             torch.save(model.state_dict(), best_model_path)
